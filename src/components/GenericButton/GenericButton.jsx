07645a8aa3f67b092a8268edbaa110ef24217f6c
Mark <hendriksen-mark@hotmail.com>
2024-04-24 22:21:39 +0200

    Update GenericButton.jsx

--- src/components/GenericButton/GenericButton.jsx
+++ src/components/GenericButton/GenericButton.jsx
@@ -1,11 +1,12 @@
 import "./genericButton.scss";
 
-const GenericButton = ({value, color, size, type}) => {
+const GenericButton = ({value, color, size, type, onClick}) => {
   return (
       <input
         type={type}
         value={value}
         className={`btn btn-block ${color} ${size}`}
+        onClick={() => onClick()}
       />
   );
 };
